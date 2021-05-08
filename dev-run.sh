7788effb196a9aa84574e5a3f7cdad710dd51bae
Abhinav <abhi.aec89@gmail.com>
2021-05-08 22:19:02 +0530

    docs(docker): environment Variables
    We learnt how to set the default env variable in docker file and how to override those while running
    the container via docker run command from command line

--- dev-run.sh
+++ dev-run.sh
@@ -1,1 +1,5 @@
 docker run --name node-app-dev -v $(pwd):/app:ro -v /app/node_modules -p 3000:3000 -d node-docker-dev
+
+# we defined a default value of PORT as 3000 in the Dockerfile.dev file but we can override via command line
+# by passing a flag called -e or --env followed by ENV_VARIABLE_NAME=ENV_VARIABLE_VALUE
+# docker run --name node-app-dev -v $(pwd):/app:ro -v /app/node_modules -e PORT=4000 -p 3000:4000 -d node-docker-dev
