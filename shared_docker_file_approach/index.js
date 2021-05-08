dfbd83dec26989c09f939696945929dd3aa25446
Abhinav <abhi.aec89@gmail.com>
2021-05-08 23:54:22 +0530

    feat(docker): docker Compose from a single Dockerfile for both Dev and Prod
    We will now be using a single docker file and override the properties in the env specific compose
    files. Details are in env specific run.sh files

---
+++ shared_docker_file_approach/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.get("/",(_,res)=>{
+    res.send("I am alive & kicking");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+console.log(`Listening on Port http://localhost:${port}`);
+}); 
+ 
