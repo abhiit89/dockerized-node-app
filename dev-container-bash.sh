2318da5ad039a4118fa9a2ee21af395a969dc70c
Abhinav <abhi.aec89@gmail.com>
2021-05-11 22:09:52 +0530

    chore(*): renamed few files
    Renamed few files

--- dev-container-bash.sh
+++ dev-container-bash.sh
@@ -1,3 +1,3 @@
-docker exec -it dockerized-node-app_node-app_1 sh
+docker exec -it dockerized-node-app_node-app_1 ls
 
 #  once inside the container run printenv to print out all the environment variables.
