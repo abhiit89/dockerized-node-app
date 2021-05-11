b3da3488aeb52b0829baa939aa455118ec04050e
Abhinav <abhi.aec89@gmail.com>
2021-05-11 23:19:42 +0530

    feat(mongodb): added the MongoDB Configuration in index.js
    added the db config

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const MONGO_PORT = parseInt(process.env.MONGO_DB_PORT) || 27017;
+const MONGO_USER = process.env.MONGO_INITDB_ROOT_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const MONGO_HOST = 'mongo';
+const mongoDBUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/?authSource=admin`;
+mongoose.connect(mongoDBUrl)
+.then(()=> console.log("Successfully connected to mongo DB"))
+.catch(()=> console.error("Error while connecting"));
 const app = express();
 app.get("/",(_,res)=>{
     res.send("I am alive & kicking");
