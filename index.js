40a4ad7218572902ce426ac89b0ca117d2611469
Abhinav <abhi.aec89@gmail.com>
2021-05-11 23:37:20 +0530

    fix(mongo): isolated the Mongo Configuration
    Isolated the MongoDB Configuration to a separate config file

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const MONGO_PORT = parseInt(process.env.MONGO_DB_PORT) || 27017;
-const MONGO_USER = process.env.MONGO_INITDB_ROOT_USERNAME;
-const MONGO_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
-const MONGO_HOST = 'mongo';
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_HOST,
+  MONGO_PORT,
+} = require("./config/config");
 const mongoDBUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/?authSource=admin`;
-mongoose.connect(mongoDBUrl)
-.then(()=> console.log("Successfully connected to mongo DB"))
-.catch(()=> console.error("Error while connecting"));
+mongoose
+  .connect(mongoDBUrl)
+  .then(() => console.log("Successfully connected to mongo DB"))
+  .catch(() => console.error("Error while connecting"));
 const app = express();
-app.get("/",(_,res)=>{
-    res.send("I am alive & kicking");
+app.get("/", (_, res) => {
+  res.send("I am alive & kicking");
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-console.log(`Listening on Port http://localhost:${port}`);
-}); 
- 
+  console.log(`Listening on Port http://localhost:${port}`);
+});
